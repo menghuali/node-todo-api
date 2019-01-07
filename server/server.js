3732cade8bf5bd794b37cdd2b07d8e4152f7a3e6
mli <menghua.li@westjet.com>
2019-01-07 13:01:03 -0500

    Add POST /todos and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc)=> {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
