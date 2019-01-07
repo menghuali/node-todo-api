cb2d424cab1d26046ff5e6e4855adbd0083c707b
mli <menghua.li@westjet.com>
2019-01-07 14:24:19 -0500

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,16 @@ const expect = require('expect');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
+const todos = [
+  {text: '1st text todo'},
+  {text: '2nd text todo'}
+];
+
 // Interesting feature. Can be used for event engine?
 beforeEach((done) => {
-  Todo.deleteMany({}).then(() => done());
+  Todo.deleteMany({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -21,7 +28,7 @@ describe('POST /todos', () => {
         return done(err);
       }
 
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -37,9 +44,34 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((err) => done(err));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app).get('/todos')
+      .expect(200)
+      .expect((res) => {
+        var _todos = res.body.todos;
+        expect(_todos.length).toBe(2);
+        // expect(_todos[0].text).toBe(todos[0].text);
+        // expect(_todos[1].text).toBe(todos[1].text);
+      }).end(done);
+      // .end((err) => {
+      //   if (err) {
+      //     return done(err);
+      //   }
+      //
+      //   Todo.find().then((_todos) => {
+      //     expect(_todos.length).toBe(2);
+      //     expect(_todos[0].text).toBe(todos[0].text);
+      //     expect(_todos[1].text).toBe(todos[1].text);
+      //     done();
+      //   }).catch((e) => done(e));
+      // });
   });
 });
