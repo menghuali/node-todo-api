13d59a6cc4f983879d047fbbdf578796333624da
mli <menghua.li@westjet.com>
2019-01-09 06:25:55 -0500

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -43,8 +45,8 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => res.status('400').send());
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log('Started on port', port);
 });
 
 module.exports = {app};
