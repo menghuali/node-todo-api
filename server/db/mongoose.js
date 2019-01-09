9111566b9abc75dad86defbe4c0000fa27858597
mli <menghua.li@westjet.com>
2019-01-09 06:48:59 -0500

    Configured connection to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+if (process.env.PORT) {
+  mongoose.connect('mongodb://user1:1QazxsW2@ds119750.mlab.com:19750/sandbox1', { useNewUrlParser: true });
+} else {
+  mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+}
 
 module.exports = {
   mongoose
