6887b22553d5b6db54c5ab2d18c2ef5c713d4482
mli <menghua.li@westjet.com>
2019-01-17 14:30:39 -0500

    Added POST /users

--- server/server.js
+++ server/server.js
@@ -79,7 +79,7 @@ app.patch('/todos/:id', (req, res) => {
       res.status(404).send();
     }
     res.send({todo});
-  }).catch(e => res.status(400).send(e));
+  }).catch(e => res.status(400).send());
 });
 
 app.post('/users', (req, res) => {
